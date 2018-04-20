a148142ca850ee830b6ef483451f3aef87fe9eab
Liu Chengzhe <mystric@foxmail.com>
2018-04-20 10:25:18 +0800

    修改配置1：utils.js文件新增函数

--- build/utils.js
+++ build/utils.js
@@ -3,7 +3,65 @@ const path = require('path')
 const config = require('../config')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const packageConfig = require('../package.json')
+/*多页应用增加内容
+* ++++++++++++++++++++++++++++++++++++++++++*/
 
+// glob是webpack安装时依赖的一个第三方模块，该模块允许你使用 *等符号, 例如lib/*.js就是获取lib文件夹下的所有js后缀名的文件
+const glob=require('glob')
+//页面模板
+const HtmlWebpackPlugin=require('html-webpack-plugin')
+// 取得相应的页面路径，因为之前的配置，所以是src文件夹下的pages文件夹
+const PAGE_PATH=path.resolve(__dirname,'../src/pages')
+const merge=require('webpack-merge')
+
+/*多入口配置
+* 通过glob模块读取pages文件夹下的所有对应文件夹下的js后缀文件，如果该文件存在
+* 那么就作为入口处理*/
+exports.entries=function(){
+  var entryFiles=glob.sync(PAGE_PATH+'/*/*.js')
+  var map={}
+  entryFiles.forEach((filePath)=>{
+    var filename=filePath.substring(filePath.lastIndexOf('\/')+1,filePath.lastIndexOf('.'))
+    map[filename]=filePath
+  })
+  return map
+}
+
+/*多页面输出配置
+* 与上面的多页面入口配置相同，读取pages文件夹下的对应的html后缀文件，然后放入数组中*/
+exports.htmlPlugins=function(){
+  let entryHtml=glob.sync(PAGE_PATH+'/*/*.html')
+  let arr=[]
+  entryHtml.forEach((filePath)=>{
+    let filename=filePath.substring(filePath.lastIndexOf('\/')+1,filePath.lastIndexOf('.'))
+    let conf={
+      //模板来源
+      template:filePath,
+      //文件名称
+      filename:filename+'.html',
+      //页面模板需要加对应的js脚本，如果不加这行则每个页面都会引入所有js脚本
+      chunks:['manifest','vendor',filename],
+      inject:true
+    }
+    //如果为生产版本，则需要minify配置
+    if(process.env.NODE_ENV==='production'){
+      conf=merge(conf,{
+        minify:{
+          removeComments:true,
+          collapseWhitespace:true,
+          removeAttributeQuotes:true,
+        },
+        chunksSortMode:'dependency'
+      })
+    }
+    arr.push(new HtmlWebpackPlugin(conf))
+  })
+  return arr
+}
+
+
+/*+++++++++++++++++++++++++++++++++++++++++++
+多页应用增加内容结束*/
 exports.assetsPath = function (_path) {
   const assetsSubDirectory = process.env.NODE_ENV === 'production'
     ? config.build.assetsSubDirectory
